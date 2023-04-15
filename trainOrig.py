69bb45082152c7bf50a7765c5a8f82de3cedc41f
Juanfma12 <124707021+Juanfma12@users.noreply.github.com>
2023-04-14 22:26:02 -0500

    Add files via upload

--- trainOrig.py
+++ trainOrig.py
@@ -3,7 +3,7 @@ sys.path.insert(0,'./keops')
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
-os.environ["USE_KEOPS"] = "False"
+os.environ["USE_KEOPS"] = "False";
 
 import pickle
 import numpy as np
@@ -43,11 +43,6 @@ random.shuffle(time_indices)
 test_indices = time_indices[:test_size]
 train_indices = time_indices[test_size:]
 
-xTr = x[train_indices]
-xTe = x[test_indices]
-
-data=x.reshape(-1, 13)
-
 def run_training_process(run_params):
     
     train_data = None
@@ -63,63 +58,15 @@ def run_training_process(run_params):
         val_data = test_data = TadpoleDataset(fold=run_params.fold, train=False,samples_per_epoch=1)
     
     if run_params.dataset == "datos":
-        #Esto crea un objeto como lo necesitamos
-        class CustomDataset(torch.utils.data.Dataset):
-            def __init__(self, data, targets, split='train', name=None, device='cuda'):
-                self.data = torch.tensor(data, dtype=torch.float32)
-                self.targets = torch.tensor(targets, dtype=torch.float32)
-                self.split = split
-                self.name = name
-                self.device = 'cuda'
-                self.n_samples = data.shape[0] 
-                self.n_features = 13
-                self.num_classes=1
-                self.edge_index = torch.tensor([(i,j) for i in range(198) for j in range(198) if i!=j], dtype=torch.long).t().cpu()
-            def __len__(self):
-                return len(self.data)
-            
-            def __getitem__(self, index):
-                sample = self.data[index]
-                target = self.targets[index]
-                
-                mask = torch.ones_like(sample)
-                mask[sample == 0] = 0
-
-                # Compute edges tensor
-                edges = self.edge_index
-                
-                return sample, target, mask, edges
-                    
-        train_data = CustomDataset(data=xTr, targets=y, split='train', name='my_dataset', device='cuda')
-        val_data = CustomDataset(data=xTe, targets=y, split='test', name='my_dataset')
-        test_data = CustomDataset(data=xTe, targets=y, split='validation', name='my_dataset')
-
-        #class CustomDataset(torch.utils.data.Dataset):
-        #    def __init__(self, data, targets):
-         #       self.data = data
-          #      self.targets = targets
-           #     
-           # def __len__(self):
-            #    return len(self.targets)
-                
-           # def __getitem__(self, index):
-            #    x = torch.FloatTensor(self.data[index])
-             #   y = torch.LongTensor([self.targets[index]])
-              #  return x, y
-        
-        
-        #### Hacer esto con Train, Test y Validation, algo de tipo:
-            #train_data=CustomDataset(xTrain, yTrain)
-            #val_data=CustomDataset(xVal, yVal)
-            #test_data=CustomDataset(xVal, yVal)
-        #custom_dataset = CustomDataset(x, y)
-        
+        train_data = datos[train_indices,:,:]
+        val_data = test_data = datos[test_indices,:,:]
+                               
     if train_data is None:
         raise Exception("Dataset %s not supported" % run_params.dataset)
         
-    train_loader = DataLoader(train_data, batch_size=8,num_workers=0)
-    val_loader = DataLoader(val_data, batch_size=8)
-    test_loader = DataLoader(test_data, batch_size=8)
+    train_loader = DataLoader(train_data, batch_size=1,num_workers=0)
+    val_loader = DataLoader(val_data, batch_size=1)
+    test_loader = DataLoader(test_data, batch_size=1)
 
     class MyDataModule(pl.LightningModule): 
         def setup(self,stage=None):
@@ -172,18 +119,19 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser = pl.Trainer.add_argparse_args(parser)
     params = parser.parse_args(['--gpus','1',                         
-                              '--log_every_n_steps','2',                          
-                              '--max_epochs','20',
-                              '--check_val_every_n_epoch','2'])
+                              '--log_every_n_steps','100',                          
+                              '--max_epochs','100',
+                              '--check_val_every_n_epoch','1'])
     parser.add_argument("--num_gpus", default=10, type=int)
     
-    parser.add_argument("--dataset", default='datos')
+    parser.add_argument("--dataset", default='Cora')
     parser.add_argument("--fold", default='0', type=int) #Used for k-fold cross validation in tadpole/ukbb
+    
     
     parser.add_argument("--conv_layers", default=[[32,32],[32,16],[16,8]], type=lambda x :eval(x))
     parser.add_argument("--dgm_layers", default= [[32,16,4],[],[]], type=lambda x :eval(x))
-    parser.add_argument("--fc_layers", default=[8,8,1], type=lambda x :eval(x))
-    parser.add_argument("--pre_fc", default=None, type=lambda x :eval(x))
+    parser.add_argument("--fc_layers", default=[8,8,3], type=lambda x :eval(x))
+    parser.add_argument("--pre_fc", default=[-1,32], type=lambda x :eval(x))
 
     parser.add_argument("--gfun", default='gcn')
     parser.add_argument("--ffun", default='gcn')
@@ -192,7 +140,7 @@ if __name__ == "__main__":
     parser.add_argument("--distance", default='euclidean')
 
     parser.add_argument("--dropout", default=0.0, type=float)
-    parser.add_argument("--lr", default=5e-1, type=float)
+    parser.add_argument("--lr", default=1e-2, type=float)
     parser.add_argument("--test_eval", default=10, type=int)
 
     parser.set_defaults(default_root_path='./log')
