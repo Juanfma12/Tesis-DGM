c5300d1b2325fe13200539064b0267acad36a640
Juanfma12 <124707021+Juanfma12@users.noreply.github.com>
2023-04-18 09:12:49 -0500

    Add files via upload

--- DGMlib/model_dDGM.py
+++ DGMlib/model_dDGM.py
@@ -97,31 +97,32 @@ class DGM_Model(pl.LightningModule):
         X, y, mask, edges = train_batch
         edges = edges[0]
         
-        assert(X.shape[0]==1) #only works in transductive setting
+        #assert(X.shape[0]==1) #only works in transductive setting
         mask=mask[0]
         
         pred,logprobs = self(X,edges)
         
-        train_pred = pred[:,mask.to(torch.bool),:]
-        train_lab = y[:,mask.to(torch.bool),:]
+        train_pred = pred
+        train_lab = y
 #         train_w = weight[None,mask.to(torch.bool)]    
 
         #loss = torch.nn.functional.cross_entropy(train_pred.view(-1,train_pred.shape[-1]),train_lab.argmax(-1).flatten())
         loss = torch.nn.functional.mse_loss(train_pred,train_lab)
         loss.backward()
 
-        correct_t = -np.square(train_pred.argmax(-1) - train_lab.argmax(-1)).float().mean().item()
-
+        correct_t = -torch.square(train_pred.cpu() - train_lab.cpu()).float().mean().item()
+        
         #GRAPH LOSS
         if logprobs is not None: 
-            corr_pred = -np.square(train_pred.argmax(-1)-train_lab.argmax(-1)).float().detach()
+            corr_pred = -torch.square(train_pred.cpu()-train_lab.cpu()).float().detach()
             wron_pred = (1-corr_pred)
 
             if self.avg_accuracy is None:
                 self.avg_accuracy = torch.ones_like(corr_pred)*0.5
 
             point_w = (self.avg_accuracy-corr_pred)#*(1*corr_pred + self.k*(1-corr_pred))
-            graph_loss = point_w * logprobs[:,mask.to(torch.bool),:].exp().mean([-1,-2])
+            point_w = point_w.to(logprobs.device)
+            graph_loss = point_w * torch.unsqueeze(logprobs.exp().mean([-1,-2]),dim = -1)
 
             graph_loss = graph_loss.mean()# + self.graph_f[0].Pr.abs().sum()*1e-3
             graph_loss.backward()
@@ -142,16 +143,15 @@ class DGM_Model(pl.LightningModule):
         X, y, mask, edges = train_batch
         edges = edges[0]
         
-        assert(X.shape[0]==1) #only works in transductive setting
+        #assert(X.shape[0]==1) #only works in transductive setting
         mask=mask[0]
         pred,logprobs = self(X,edges)
-        pred = pred.softmax(-1)
         for i in range(1,self.hparams.test_eval):
             pred_,logprobs = self(X,edges)
-            pred+=pred_.softmax(-1)
-        test_pred = pred[:,mask.to(torch.bool),:]
-        test_lab = y[:,mask.to(torch.bool),:]
-        correct_t = -np.square(test_pred.argmax(-1) - test_lab.argmax(-1)).float().mean().item()
+    
+        test_pred = pred
+        test_lab = y
+        correct_t = -torch.square(test_pred.cpu() - test_lab.cpu()).float().mean().item()
         loss = torch.nn.functional.mse_loss(test_pred,test_lab)
         self.log('test_loss', loss.detach().cpu())
         # self.log('test_graph_loss', loss.detach().cpu())
@@ -162,18 +162,17 @@ class DGM_Model(pl.LightningModule):
         edges = edges[0]
         
         
-        assert(X.shape[0]==1) #only works in transductive setting
+        #assert(X.shape[0]==1) #only works in transductive setting
         mask=mask[0]
         
         pred,logprobs = self(X,edges)
-        pred = pred.softmax(-1)
+        
         for i in range(1,self.hparams.test_eval):
             pred_,logprobs = self(X,edges)
-            pred+=pred_.softmax(-1)
             
-        test_pred = pred[:,mask.to(torch.bool),:]
-        test_lab = y[:,mask.to(torch.bool),:]
-        correct_t = -np.square(test_pred.argmax(-1) - test_lab.argmax(-1)).float().mean().item()
+        test_pred = pred
+        test_lab = y
+        correct_t = -torch.square(test_pred.cpu() - test_lab.cpu()).float().mean().item()
         loss = torch.nn.functional.mse_loss(test_pred,test_lab)
         
         self.log('val_loss', loss.detach())
@@ -200,4 +199,3 @@ class DGM_Model(pl.LightningModule):
        # except:
        #      pass
       
-
